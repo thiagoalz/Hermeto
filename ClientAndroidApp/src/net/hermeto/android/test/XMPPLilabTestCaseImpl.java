49dab41fb912355e6aa4b451887b5ff64a3801ef
Paulo Augusto Dacach Bichara <contato@paulobichara.org>
2011-08-18 11:39:54 -0300

    Several modifications.. Re-factoring, added XPMM handlers. JUnit Tests aren't working for some reason. I'll take a look at this later today.

---
+++ ClientAndroidApp/src/net/hermeto/android/test/XMPPLilabTestCaseImpl.java
@@ -0,0 +1,56 @@
+package net.hermeto.android.test;
+
+import junit.framework.TestCase;
+import net.hermeto.android.main.XMPPClient;
+
+import org.jivesoftware.smack.XMPPException;
+
+import dalvik.annotation.TestTargetClass;
+
+import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
+
+@TestTargetClass(value = XMPPClient.class)
+public class XMPPLilabTestCaseImpl extends TestCase {
+	
+	private XMPPClient client;	
+
+	public XMPPLilabTestCaseImpl(String name) {
+		super(name);
+	}
+	
+	@SmallTest
+	public void testLilabServer() {
+
+		String msg = "Test message;";
+
+		try {
+			this.client.login("a@lilab.info", "123456", "lilab.info", 5222);
+		} catch (XMPPException e) {
+			Log.e("hermeto","XMPP Authentication Failed");
+			e.printStackTrace();
+		}
+
+		try {
+			this.client.sendMessage(msg);
+		} catch (XMPPException e) {
+			Log.e("hermeto","XMPP Message Sending Failed");			
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	@Override
+	protected void tearDown() throws Exception {
+		this.client.disconnect();
+		super.tearDown();
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.client = new XMPPClient();
+	}
+
+}
