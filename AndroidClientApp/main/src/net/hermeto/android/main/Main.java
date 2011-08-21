ca32bf6ab93834f1341f7b604c6896892e0f41c2
Daniel Vecchiato <vecchiato86@gmail.com>
2011-08-21 15:42:42 -0300

    Refactoring da main activity
    Signed-off-by: Thiago Alvarenga Lechuga <thiagoalz@gmail.com>

--- AndroidClientApp/main/src/net/hermeto/android/main/Main.java
+++ AndroidClientApp/main/src/net/hermeto/android/main/Main.java
@@ -5,139 +5,166 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class Main extends Activity {
-	
+
 	ConnectionController mConnectionController;
 	Toast ConnToast;
-		
+
+	private ImageButton btnUp;
+	private ImageButton btnLeft;
+	private ImageButton btnDown;
+	private ImageButton btnRight;
+	private ImageButton btnConnect;
+	private ImageButton btnCenter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 
-		//Setup Controller
+		// Setup Controller
 		mConnectionController = new ConnectionController(Main.this);
-		
-		ConnToast=Toast.makeText(Main.this, "Connected!", Toast.LENGTH_LONG);
+
+		ConnToast = Toast.makeText(Main.this, "Connected!", Toast.LENGTH_LONG);
 		Window janela = getWindow();
 		janela.requestFeature(Window.FEATURE_LEFT_ICON);
 		setContentView(R.layout.main);
 		janela.setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
 				R.drawable.icon);
 
-		// / When b_up button clicked do ...
-		final ImageButton b_Up = (ImageButton) findViewById(R.id.b_up);
-		b_Up.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-
-				mConnectionController.upClick();
-
-			}
-		});
-
-		// When b_left button clicked do ...
-		final ImageButton b_Left = (ImageButton) findViewById(R.id.b_left);
-		b_Left.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-
-				mConnectionController.leftClick();
-
-			}
-		});
-
-		// When b_right button clicked do ...
-		final ImageButton b_Right = (ImageButton) findViewById(R.id.b_right);
-		b_Right.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-
-				mConnectionController.rightClick();
-
-			}
-		});
-
-		// When b_down button clicked do ...
-		final ImageButton b_Down = (ImageButton) findViewById(R.id.b_down);
-		b_Down.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-
-				mConnectionController.downClick();
-
-			}
-		});
-
-		// When b_center button clicked do ...
-		final ImageButton b_Center = (ImageButton) findViewById(R.id.b_center);
-		b_Center.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				mConnectionController.buttonClick();
-			}
-		});
-
-		// When b_connect button clicked do ...
-		final ImageButton b_Connect = (ImageButton) findViewById(R.id.b_connect);
-		b_Connect.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-
-				AlertDialog.Builder dialogo = new AlertDialog.Builder(Main.this);
-				dialogo.setTitle(R.string.d_title);
-				dialogo.setIcon(R.drawable.net_fail);
-				dialogo.setMessage(R.string.d_text);
-
-				final EditText input = new EditText(Main.this);
-				input.setSingleLine();
-
-				dialogo.setView(input);				
-				
-				dialogo.setPositiveButton(R.string.d_ok,
-						new DialogInterface.OnClickListener() {
-
-					@Override
-					public void onClick(DialogInterface dialog,
-							int which) {
-						
-						String nickname = input.getText().toString();
-
-						mConnectionController.connect(nickname);
-
-
-					}
-				});
-
-				dialogo.setNegativeButton(R.string.d_cancel,
-						new DialogInterface.OnClickListener() {
-
-					@Override
-					public void onClick(DialogInterface dialog,
-							int which) {
-
-
-					}
-				});
-
-				dialogo.show();
-
-			}
-		});
+		bindViews();
+		bindEvents();
 
 	}
+
+	/********
+	 * Binders
+	 **********/
+	public void bindViews() {
+
+		// Controls
+		btnUp = (ImageButton) findViewById(R.id.b_up);
+		btnLeft = (ImageButton) findViewById(R.id.b_left);
+		btnDown = (ImageButton) findViewById(R.id.b_down);
+		btnRight = (ImageButton) findViewById(R.id.b_right);
+		btnCenter = (ImageButton) findViewById(R.id.b_center);
+
+		// Connection
+		btnConnect = (ImageButton) findViewById(R.id.b_connect);
+	}
+
+	public void bindEvents() {
+
+		btnUp.setOnClickListener(mBtnUpClick);
+		btnLeft.setOnClickListener(mBtnLeftClick);
+		btnDown.setOnClickListener(mBtnDownClick);
+		btnRight.setOnClickListener(mBtnRightClick);
+		btnCenter.setOnClickListener(mBtnCenterClick);
+		btnConnect.setOnClickListener(mBtnConnectClick);
+
+	}
+
+	/***********
+	 * OnClickListeners
+	 *********/
+	OnClickListener mBtnUpClick = new OnClickListener() {
+
+		public void onClick(View v) {
+
+			mConnectionController.upClick();
+
+		}
+	};
+
+	OnClickListener mBtnLeftClick = new OnClickListener() {
+
+		public void onClick(View v) {
+
+			mConnectionController.leftClick();
+
+		}
+	};
+
+	OnClickListener mBtnDownClick = new OnClickListener() {
+
+		public void onClick(View v) {
+
+			mConnectionController.downClick();
+
+		}
+	};
+
+	OnClickListener mBtnRightClick = new OnClickListener() {
+
+		public void onClick(View v) {
+
+			mConnectionController.rightClick();
+
+		}
+	};
 	
-	//TODO: COuldnt find another way to controller start the toast.
-	//This way just works the 1 time :/
-	public void toastConnected(){
+	OnClickListener mBtnCenterClick = new OnClickListener() {
+
+		public void onClick(View v) {
+			mConnectionController.buttonClick();
+		}
+	};
+
+	OnClickListener mBtnConnectClick = new OnClickListener() {
+
+		public void onClick(View v) {
+
+			AlertDialog.Builder dialogo = new AlertDialog.Builder(Main.this);
+			dialogo.setTitle(R.string.d_title);
+			dialogo.setIcon(R.drawable.net_fail);
+			dialogo.setMessage(R.string.d_text);
+
+			final EditText input = new EditText(Main.this);
+			input.setSingleLine();
+
+			dialogo.setView(input);
+
+			dialogo.setPositiveButton(R.string.d_ok,
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog,
+								int which) {
+
+							String nickname = input.getText().toString();
+							Log.e("XMPP", "Button pressed");
+							mConnectionController.connect(nickname);
+
+						}
+					});
+
+			dialogo.setNegativeButton(R.string.d_cancel,
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog,
+								int which) {
+
+						}
+					});
+
+			dialogo.show();
+
+		}
+	};
+	
+	// TODO: COuldnt find another way to controller start the toast.
+	// This way just works the 1 time :/
+	public void toastConnected() {
 		ConnToast.show();
 	}
-		
+
 }
